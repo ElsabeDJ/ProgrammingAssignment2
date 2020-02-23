d6fb80e52ef87d52d4259a704708ad46a1341fa9
ElsabeDJ <mail.elsabe@gmail.com>
2020-02-23 18:15:01 +0200

    Changed the functions as specified in the coursera assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache the inverse of a mtarix
 
-## Write a short comment describing this function
+## Function to create matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Initialize the inverseMatrix 
+  inverseMatrix <- NULL
+  
+  ##  Define the get & set functions for the inverseMatrix 
+  set <- function(y) {
+      x <<- y
+      inverseMatrix <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) inverseMatrix <<- solve
+  getInverse <- function () inverseMatrix
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Function to create and inverse of the matrix if it does not already exists.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Get the cached value for the inverseMatrix
+  inverseMatrix <- x$getInverse()
+  if (!is.null(inverseMatrix)) {
+    message("Getting the cached data for the inverse of the matrix")
+    return(inverseMatrix)
+  }
+  data <- x$get()
+  inverseMatrix <- solve(data)
+  x$setInverse(inverseMatrix)
+  inverseMatrix
 }
